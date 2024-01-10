5afa033b04621be0d2310922dbbf33dcb7c5ad97
devhiteshk <hitesh.k.83080@gmail.com>
2024-01-10 14:32:15 +0530

    test push atoms

--- packages/random-string/lib/random-string.js
+++ packages/random-string/lib/random-string.js
@@ -2,6 +2,7 @@
 
 const charactersSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 // function returns random str
+// hello world
 // 1
 function randomString(len = 5) {
   const str = [...Array(len)]
