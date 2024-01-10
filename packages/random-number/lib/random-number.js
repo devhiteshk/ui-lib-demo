d09aa156d141ff9b5ce0abd6f5a14ca1f76cefb6
devhiteshk <hitesh.k.83080@gmail.com>
2024-01-10 14:02:28 +0530

    setup organisms

--- packages/random-number/lib/random-number.js
+++ packages/random-number/lib/random-number.js
@@ -1,10 +1,10 @@
 "use strict";
 //function return a random num
 // 1
-function randomNumber(min, max) {
+function randomNumberDivide(min, max) {
   const num = Math.floor(Math.random() * (max - min) + min);
-  return num;
+  return num / 100;
 }
 
 // 2
-module.exports = randomNumber;
+module.exports = randomNumberDivide;
